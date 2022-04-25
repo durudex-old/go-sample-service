9a05f70c82f85e1cc2167a0f74931460f465740e
V1def <vaniya8871@gmail.com>
2022-04-25 13:55:43 +0300

    Update default gRPC server options

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -34,18 +34,17 @@ type gRPCServer struct{ Server *grpc.Server }
 func NewGRPC(cfg *config.TLSConfig) (*gRPCServer, error) {
 	serverOptions := []grpc.ServerOption{}
 
+	// Added basic server options.
+	serverOptions = append(serverOptions, grpc.UnaryInterceptor(unaryInterceptor))
+
 	if cfg.Enable {
 		creds, err := tls.LoadTLSCredentials(cfg.CACert, cfg.Cert, cfg.Key)
 		if err != nil {
 			return nil, err
 		}
 
-		// Append server options.
-		serverOptions = append(
-			serverOptions,
-			grpc.Creds(creds),
-			grpc.UnaryInterceptor(unaryInterceptor),
-		)
+		// Append server credential options.
+		serverOptions = append(serverOptions, grpc.Creds(creds))
 	}
 
 	return &gRPCServer{Server: grpc.NewServer(serverOptions...)}, nil
